a87a3dc5ffd014c3611bcfbc6f3d360bc9d5a165
Suryabhan Singh <suryabhan283808@gmail.com>
2025-07-09 19:28:35 +0530

    Refactor code structure for improved readability and maintainability

--- register/app/views.py
+++ register/app/views.py
@@ -9,9 +9,9 @@ def registerdata(req):
     if req.method=='POST':
        n=req.POST.get('fname')
        e=req.POST.get('email')
-       i=req.POST.get('image')
+       i=req.FILES.get('image')
        c=req.POST.get('contact')
-       d=req.POST.get('document')
+       d=req.FILES.get('document')
        print(n, e, i, c, d, sep=',') 
        Student.objects.create(fname=n, email=e, contact=c, image=i, document=d)
        return HttpResponse("registration done")
